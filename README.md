ef933947ab2f47af1f6d1df2e1cca3dbfddb1ef2
charltonaustin <charlton.austin@kin.com>
2024-08-18 04:33:59 -0700

    Adding in a warning if missing github access token.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # GitHub Contribution Counter (GHCC)
+This is a small program that allows a user to display contributions made by GitHub users.
+The basic flow is something like.
+- Add in an org.
+- Refresh users for that org.
+- Add in a repo.
+- Refresh prs, commits, and reviews.
+- Display contributions for your users.
+
 ## Dependencies
 For ruby please see the `.ruby-version` file
 ## To setup
--- lib/shared/github_client.rb
+++ lib/shared/github_client.rb
@@ -3,7 +3,12 @@
 require 'octokit'
 ACCESS_TOKEN = ENV.fetch('GHCC_ACCESS_TOKEN', nil)
 
+def warn_missing_access_token
+  puts 'MISSING ACCESS TOKEN SO COMMANDS WILL ONLY WORK ON PUBLIC REPOS'
+end
+
 def git_client
+  warn_missing_access_token if ACCESS_TOKEN.nil?
   client = Octokit::Client.new(access_token: ACCESS_TOKEN)
   client.default_media_type = 'application/vnd.github+json'
   client.auto_paginate = true
