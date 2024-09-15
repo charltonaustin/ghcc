3acca806dee1832ab6ef317d57bb4ac19eae1b78
charltonaustin <charlton.austin@kin.com>
2024-09-15 14:21:30 -0700

    Adding in some tests to get a better sense of the code that isn't covered.

---
+++ lib/commands/contributions/process_contributions_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative 'process_contributions'
+
+RSpec.describe 'process_contributions' do
+  it 'print_' do
+    expect(true).to be_truthy
+  end
+end
---
+++ lib/commands/contributions/user_details_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative 'user_details'
+
+RSpec.describe 'user_details' do
+  it 'format_commits' do
+    expect(true).to be_truthy
+  end
+end
