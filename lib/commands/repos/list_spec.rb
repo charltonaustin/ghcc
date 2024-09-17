931f041781c45fb88ce65a56b801858cad05dc03
charltonaustin <charlton.austin@kin.com>
2024-09-17 16:36:00 -0700

    Adding in some tests to get a better sense of the code that isn't covered.

---
+++ lib/commands/repos/list_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative 'list'
+
+RSpec.describe 'list' do
+  it 'list_repos' do
+    expect(true).to be_truthy
+  end
+end
---
+++ lib/commands/repos/toggle_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative 'toggle'
+
+RSpec.describe 'toggle' do
+  it 'toggle_repo' do
+    expect(true).to be_truthy
+  end
+end
