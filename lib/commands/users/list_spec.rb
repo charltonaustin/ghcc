1ccb3eb8e6a5ac495a2205fc3f46ad8253a3174d
charltonaustin <charlton.austin@gmail.com>
2024-10-02 09:17:58 -0700

    Adding in a test to increase test coverage
    Signed-off-by: charltonaustin <charlton.austin@gmail.com>

--- lib/commands/users/list_spec.rb
+++ lib/commands/users/list_spec.rb
@@ -3,6 +3,13 @@
 require 'rspec'
 require_relative 'list'
 
+def given_repository_returns_mixed_users
+  allow(Users::Repository).to receive(:get_users).and_return [
+    { user_name: 'username', name: 'name',
+      to_process: true }, { user_name: 'username1', name: 'name1', to_process: false }
+  ]
+end
+
 RSpec.describe 'List' do
   context 'when list_users_from is called' do
     it 'displays all users' do
@@ -19,6 +26,13 @@ RSpec.describe 'List' do
       expect do
         Users::List.list_users_from(object_double('db').as_null_object, 'name', false)
       end.to output(/Username: username, name: name, to_process: false/).to_stdout
+    end
+
+    it 'displays matched users for processable users' do
+      given_repository_returns_mixed_users
+      expect do
+        Users::List.list_users_from(object_double('db').as_null_object, 'name', true)
+      end.to output(/Username: username, name: name, to_process: true/).to_stdout
     end
   end
 end
